c793fcc9b2fa5975a553d9419ae0f0b2ab20e172
Frida Alberte Bendorff <frbe0002@stud.kea.dk>
2024-11-14 12:10:30 +0100

    padding added to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,10 +10,12 @@ const Header = () => {
 
   const handleClick = () => {
     setIsOpen(!isOpen);
-  }
+  };
 
   return (
-    <header className={`py-2 bg-main-background drop-shadow-main transition-all duration-500`}>
+    <header
+      className={`py-4 bg-main-background drop-shadow-main transition-all duration-500`}
+    >
       <nav className="flex justify-between">
         <div className="flex gap-6">
           <Link href="/">Logo</Link>
@@ -28,32 +30,39 @@ const Header = () => {
             <HiOutlineShoppingCart size="24px" />
           </button>
           <button onClick={handleClick} className="z-20 md:hidden">
-            <span  className={`bg-silver-chalice-900 block transition-all duration-300 ease-out 
-                h-0.5 w-6 rounded-sm ${isOpen ? 
-                'rotate-45 translate-y-1.5' : '-translate-y-0.5'
-                }`}>
-            </span>
-            
-            <span className={`bg-silver-chalice-900 block transition-all duration-300 ease-out 
-                h-0.5 w-6 rounded-sm my-1 ${isOpen ? 
-                'opacity-0' : 'opacity-100'
-                }`} >
-            </span>
+            <span
+              className={`bg-silver-chalice-900 block transition-all duration-300 ease-out 
+                h-0.5 w-6 rounded-sm ${
+                  isOpen ? "rotate-45 translate-y-1.5" : "-translate-y-0.5"
+                }`}
+            ></span>
 
-            <span className={`bg-silver-chalice-900 block transition-all duration-300 ease-out 
-                h-0.5 w-6 rounded-sm ${isOpen ? 
-                '-rotate-45 -translate-y-1.5' : 'translate-y-0.5'
-                }`} >
-            </span>
+            <span
+              className={`bg-silver-chalice-900 block transition-all duration-300 ease-out 
+                h-0.5 w-6 rounded-sm my-1 ${
+                  isOpen ? "opacity-0" : "opacity-100"
+                }`}
+            ></span>
+
+            <span
+              className={`bg-silver-chalice-900 block transition-all duration-300 ease-out 
+                h-0.5 w-6 rounded-sm ${
+                  isOpen ? "-rotate-45 -translate-y-1.5" : "translate-y-0.5"
+                }`}
+            ></span>
           </button>
         </menu>
       </nav>
-      
-      <div className={`w-screen h-screen absolute z-10 top-0 left-[-16px] bg-silver-chalice-100 flex flex-col items-center justify-around transition-all duration-500 ease-in-out ${isOpen ? 'left-0' : 'left-full'}`}>
-            <Link href="/">Logo</Link>
-            <Link href="/">Home</Link>
-            <Link href="/">Products</Link>
-          </div>
+
+      <div
+        className={`w-screen h-screen absolute z-10 top-0 left-[-16px] bg-silver-chalice-100 flex flex-col items-center justify-around transition-all duration-500 ease-in-out ${
+          isOpen ? "left-0" : "left-full"
+        }`}
+      >
+        <Link href="/">Logo</Link>
+        <Link href="/">Home</Link>
+        <Link href="/">Products</Link>
+      </div>
     </header>
   );
 };
