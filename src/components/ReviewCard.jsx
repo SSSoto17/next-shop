b75d8f4d5ef21ad3d4ab79ebb1876302d609cc34
Sif Melkane <siha0004@stud.kea.dk>
2024-11-15 13:03:16 +0100

    Soto playing around - until Sif parked it

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,16 +1,33 @@
+"use client";
+
+import { useState } from "react";
+
 import { FaStar } from "react-icons/fa";
+
+const starVariants = {
+  default: "text-silver-chalice-300",
+  active: "",
+};
 
 const ReviewCard = ({ data }) => {
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     return new Intl.DateTimeFormat("da-DK", { day: "2-digit", month: "2-digit", year: "numeric" }).format(date);
   };
+  const [starRating, setStarRating] = useState([]);
+
+  function calcRating(rating) {
+    const activeStar = active;
+
+    setStarRating([activeStar] * rating);
+  }
 
   return (
     <article className="py-3 px-2 border-b-2 border-silver-chalice-100">
       <header className="flex justify-between">
         <h4 className="font-bold">{data.reviewerName}</h4>
         <div className="flex pt-1">
+          {star.repeat(data.rating)}
           <FaStar className="text-silver-chalice-300" />
           <FaStar className="" />
           <FaStar />
