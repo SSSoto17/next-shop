a36adee3d25b8953ebed60f9efa836ed5df98a8f
Frida Alberte Bendorff <frbe0002@stud.kea.dk>
2024-11-15 12:08:43 +0100

    gap adjusted on productGrid

--- src/components/products/ProductGrid.jsx
+++ src/components/products/ProductGrid.jsx
@@ -2,7 +2,7 @@ import ProductCard from "./ProductCard";
 
 const ProductGrid = ({ data, addToBasket }) => {
   return (
-    <section className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-4">
+    <section className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
       {data.map((product) => {
         return (
           <ProductCard
