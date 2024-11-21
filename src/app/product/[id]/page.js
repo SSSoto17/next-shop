f4fe4d19e41a0e2ddc1152a99f08372b6d77345b
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2024-11-21 11:54:00 +0100

    more info deleted

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
 
-import MoreInfo from "@/components/MoreInfo";
+// import MoreInfo from "@/components/MoreInfo";
 import ImageGallery from "@/components/products/singleproduct/ImageGallery";
 import { MdOutlineArrowBack } from "react-icons/md";
 
 export default async function ProductSingle({ params }) {
   const id = (await params).id;
-  let product = await fetch(`https://dummyjson.com/products/${id}`).then((res) => res.json());
+  let product = await fetch(`https://dummyjson.com/products/${id}`).then(
+    (res) => res.json()
+  );
 
   return (
     <main>
@@ -21,7 +23,9 @@ export default async function ProductSingle({ params }) {
       <section className="grid md:grid-cols-2 gap-8">
         <ImageGallery product={product}></ImageGallery>
         <article className="flow-space">
-          <p className="w-fit py-2 px-6 -ml-3 top-3 bg-tabasco-800 rounded-lg text-silver-chalice-50">Save {product.discountPercentage}%</p>
+          <p className="w-fit py-2 px-6 -ml-3 top-3 bg-tabasco-800 rounded-lg text-silver-chalice-50">
+            Save {product.discountPercentage}%
+          </p>
           <h1 className="text-silver-chalice-600 text-xl">{product.brand}</h1>
           <h2 className="text-silver-chalice-950 text-4xl">{product.title}</h2>
           <p className="text-2xl pb-8">
